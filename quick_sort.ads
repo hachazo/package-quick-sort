441ef48cbba727870b7c6a4af7daa60cfe7335e9
hachazo <juliodias0297@gmail.com>
2023-06-09 07:19:37 -0300

    update

--- quick_sort.ads
+++ quick_sort.ads
@@ -19,5 +19,36 @@ generic
       procedure Dividir (Datos: in out Tipovecc; Primero, Ultimo: in indice; Puntodivision: in out Indice);
       procedure Ordrapida (Datos: in out Tipovecc; Primero, Ultimo: in indice);
       
-   end Quick_sort;
+   end Quick_Sort;
    
+--   subtype Indice is Integer range 1..8;
+--   
+--   procedure Putinc(X: in Integer) is
+--   begin
+--      put(x);
+--      end putinc;
+--   
+--   procedure Getinc(X: out Integer) is
+--   begin
+--      Get(X);
+--      end getinc;
+--   
+--   function Menor(X,Y: Integer) return Boolean is
+--   begin
+--      return x<Y;
+--   end Menor;
+--   
+--   function Menorigual(X,Y: Integer) return Boolean is
+--   begin
+--      return x<=Y;
+--   end Menorigual;
+--   
+--   function Mayor(X,Y: Integer) return Boolean is
+--   begin
+--      return x>Y;
+--   end Mayor;
+--   
+--   package quickinc is new quick_Sort(integer,indice,putinc,getinc,menor,menorigual,mayor);
+--   use quickinc;
+--   vector:tipovecc;
+--   
